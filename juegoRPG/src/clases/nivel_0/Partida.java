627ccc02463c52ce997f2e71ef82b7a0b6de03e2
Victor Benito <victorbenito8@gmail.com>
2025-05-09 00:35:58 +0200

    partida acabado

--- juegoRPG/src/clases/nivel_0/Partida.java
+++ juegoRPG/src/clases/nivel_0/Partida.java
@@ -1,5 +1,37 @@
 package clases.nivel_0;
 
+import clases.nivel_1.Party;
+
 public class Partida {
- // ola
+  private Party partyJugador;
+  private Party partyEnemiga;
+
+  public Partida(Party partyJugador, Party partyEnemiga) {
+      this.partyJugador = partyJugador;
+      this.partyEnemiga = partyEnemiga;
+  }
+
+  public void iniciarJuego() {
+    Ronda ronda = new Ronda(partyJugador, partyEnemiga);
+    while (!partyJugador.isPartyEmpty()) {
+        ronda.ejecutarTurno();
+        if (partyEnemiga.isPartyEmpty()) {
+            System.out.println("La party enemiga ha sido derrotada.");
+            ronda.pasarSiguienteFase();
+        }
+        if (partyJugador.isPartyEmpty()) {
+            System.out.println("La party del jugador ha sido derrotada. Fin del juego.");
+            break;
+        }
+    }
+    ronda.terminarJuego();
+}
+
+  public Party getPartyJugador() {
+      return partyJugador;
+  }
+
+  public Party getPartyEnemiga() {
+      return partyEnemiga;
+  }
 }
